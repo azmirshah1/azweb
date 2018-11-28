d77e26008478f69e5d8ab9fc9c4aa0c2efc0c26c
Azmir Shah <azmirshah1@gmail.com>
2018-11-29 03:30:05 +0800

    Delete all the useless file

--- Journey v2.0.7 – Personal WordPress Blog Theme/journey-207/journey/template/header/header-style2.php
+++
@@ -1,9 +0,0 @@
-<!-- ========================================
-     Color Background
-========================================-->
-
-<div class="semple_header">
-	<div class="bg-logo-container" style="background-color: <?php echo esc_attr($ilgelo_options['ilgelo-bg-logo']) ?>;">
-          <?php  include(TEMPLATEPATH."/template/header/header-logo.php"); ?>
-	</div><!-- end bg-logo-container -->
-</div><!-- end semple_header -->
